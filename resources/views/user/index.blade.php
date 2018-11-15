f78070e76452b911fe19bb35ada4289d122325e4
im1971 <im1971cse@gmail.com>
2018-11-15 17:56:45 +0600

    sidebar added

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -24,11 +24,12 @@
 						@include('user.user-sidebar')
 					</div>
 					<div class="col-lg-7 col-md-7">
-						<form class="form-inline">
+					@include('user.blog-body')
+						<!-- <form class="form-inline">
 					      <div class="md-form" style="width: 100%;">
 					        <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" style="width: 100%;">
 					      </div>
-					    </form>
+					    </form> -->
 						<div class="row clearfix user_body_wrap">
 							<div class="col-md-4">
 					            <div class="card">
@@ -142,112 +143,7 @@
 							</div>
 						</div>
 					<div class="col-md-3">
-						<div class="right_sidebar">
-							<div class="all-batch">
-								<div class="card">
-								  <div class="card-header">See Other Batch</div>
-								  <div class="card-body">
-								  	<ul>
-									<a href="#">
-										<li>
-											<p>Batch: 2007</p>
-										</li>
-									</a>
-									<a href="#">
-										<li>
-											<p>Batch: 2007</p>
-										</li>
-									</a>
-									<a href="#">
-										<li>
-											<p>Batch: 2007</p>
-										</li>
-									</a>
-									<a href="#">
-										<li>
-											<p>Batch: 2007</p>
-										</li>
-									</a>
-									<a href="#">
-										<li>
-											<p>Batch: 2007</p>
-										</li>
-									</a>
-									<a href="#">
-										<li>
-											<p>Batch: 2007</p>
-										</li>
-									</a>
-									<a href="#">
-										<li>
-											<p>Batch: 2007</p>
-										</li>
-									</a>
-									<a href="#">
-										<li>
-											<p>Batch: 2007</p>
-										</li>
-									</a>
-									<a href="#">
-										<li>
-											<p>Batch: 2007</p>
-										</li>
-									</a>
-									<a href="#">
-										<li>
-											<p>Batch: 2007</p>
-										</li>
-									</a>
-									<a href="#">
-										<li>
-											<p>Batch: 2007</p>
-										</li>
-									</a>
-									<a href="#">
-										<li>
-											<p>Batch: 2007</p>
-										</li>
-									</a>
-									<a href="#">
-										<li>
-											<p>Batch: 2007</p>
-										</li>
-									</a>
-									<a href="#">
-										<li>
-											<p>Batch: 2007</p>
-										</li>
-									</a>
-									<a href="#">
-										<li>
-											<p>Batch: 2007</p>
-										</li>
-									</a>
-									<a href="#">
-										<li>
-											<p>Batch: 2007</p>
-										</li>
-									</a>
-									<a href="#">
-										<li>
-											<p>Batch: 2007</p>
-										</li>
-									</a>
-									<a href="#">
-										<li>
-											<p>Batch: 2007</p>
-										</li>
-									</a>
-								</ul>
-							  </div>
-							  <div class="card-footer">
-							</div>
-								
-							</div>
-						</div>
-						<div class="mx-h-350" style="margin-top:40px;background-color: #000 ">
-							<a href=""><img src="img/add.jpg" style="width: 100%"></a>
-						</div>
+					@include('user.blog-right-sidebar')
 					</div>
 				</div>
 			<!-- </div> -->
